d93b0a0b326e3618b9125de4bbddcf25f63a547d
Prakhar Dubey <prakhardubey2002@gmail.com>
2024-09-20 12:56:16 +0530

    cors added

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const profileRoutes = require('./routes/profile')
-const reviewRoutes = require('./routes/review')
-const cors = require('cors')
-const app = express()
-app.use(
-  cors({
-    origin: 'https://latentreviews.vercel.app',
-  })
-)
+const express = require('express');
+const mongoose = require('mongoose');
+const profileRoutes = require('./routes/profile');
+const reviewRoutes = require('./routes/review');
+const cors = require('cors');
+const app = express();
+
+// Enable CORS for all origins
+app.use(cors());
+
 // Middleware
-app.use(express.json())
+app.use(express.json());
 
 // Connect to MongoDB
 mongoose
@@ -22,14 +21,14 @@ mongoose
     }
   )
   .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Routes
-app.use('/api/profiles', profileRoutes)
-app.use('/api/reviews', reviewRoutes)
+app.use('/api/profiles', profileRoutes);
+app.use('/api/reviews', reviewRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
